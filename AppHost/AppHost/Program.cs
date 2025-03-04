72aa6854ce847e7bce19f55eea37f2ed818cd1d0
minhnv <nminh.dev@gmail.com>
2025-03-04 16:52:06 +0700

    init source

---
+++ AppHost/AppHost/Program.cs
@@ -0,0 +1,29 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Thêm Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.RoutePrefix = string.Empty; // Đặt Swagger UI làm trang mặc định
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
